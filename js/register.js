ffc531db81c128266c3bff4e773ae6b9e6168ef5
trista <trista_mccleary@yahoo.com>
2018-10-07 10:31:04 -0400

    added service worker register

---
+++ js/register.js
@@ -0,0 +1,6 @@
+/* Set up service worker here */
+navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  console.log('Yay! Service worker successful ' + reg.scope);
+}).catch(function(err) {
+  console.log('Boo! Registration failed: ' + error);
+});
