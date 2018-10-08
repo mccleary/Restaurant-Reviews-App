dfad957b20437036e272b3631a6a602f748760bf
trista <trista_mccleary@yahoo.com>
2018-10-08 13:03:57 -0400

    revised caching files

--- sw.js
+++ sw.js
@@ -5,17 +5,24 @@ self.addEventListener('install', function(event) {
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
-        'index.html',
-        'restaurant.html',
-        '/css/styles.css',
-        '/css.responsive.css',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/data/restaurants.json',
-        '/js/register.js',
-        'sw.js',
-        '/img/*'
+        '/index.html',
+        '/restaurant.html',
+        'css/styles.css',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'data/restaurants.json',
+        'js/register.js',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
       ]);
     })
   );
