e60aef02e6d0b27f4d42c9601d1eca9f40240746
trista <trista_mccleary@yahoo.com>
2018-10-08 13:06:50 -0400

    added setAttribute for images

--- js/register.js
+++ js/register.js
@@ -2,5 +2,18 @@
 navigator.serviceWorker.register('/sw.js').then(function(reg) {
   console.log('Yay! Service worker successful ' + reg.scope);
 }).catch(function(err) {
-  console.log('Boo! Registration failed: ' + error);
+  console.log('Boo! Registration failed: ' + err);
 });
+
+
+
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//   .register("/sw.js")
+//   .then(reg => {
+//     console.log("Service worker Reg successful: " + reg.scope);
+//   })
+//   .catch(error => {
+//     console.log("Registration failed: " + error);
+//   });
+// }
